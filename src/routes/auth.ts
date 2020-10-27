f50aff78b56595d5cfae12fecc96b9465615626e
PalumboN <nahuel.palumbo@gmail.com>
2020-10-27 16:41:13 -0300

    Middlewares

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,7 +2,8 @@ import * as express from 'express'
 import { syncHandler } from './utils'
 import { DocumentType } from '@typegoose/typegoose'
 import UserModel, { User } from '../models/user'
-import { generatePassword, verifyPassword, generateToken, parseToken } from '../models/auth'
+import { generatePassword, verifyPassword, generateToken } from '../models/auth'
+import { tokenAuth } from './middlewares'
 
 type AuthteticatedRequest = express.Request & { user: DocumentType<User> }
 
@@ -20,15 +21,6 @@ router.post('/login', syncHandler(async ({ body }: AuthteticatedRequest, res) =>
   const token = generateToken({ username: user.credentials.username })
   res.json({ token, ...user.profile })
 }))
-
-const tokenAuth = syncHandler(async (req: AuthteticatedRequest, res, next) => {
-  const queryparams = req.query as any
-  const { username } = parseToken(queryparams['access_token'])
-  const user = await UserModel.findByUsername(username).exec()
-  if (!user) throw "Fallo de sesión"
-  req.user = user
-  next()
-})
 
 router.get('/profile', tokenAuth, syncHandler(async ({ user }: AuthteticatedRequest, res) => {
   res.json(user.profile)
