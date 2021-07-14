42bc430285b5e8981069e2459904133aa2f82cd8
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2021-07-14 14:08:31 -0300

    fixing password in user

--- test/sessionMock.ts
+++ test/sessionMock.ts
@@ -1,10 +1,11 @@
+import { User } from "pilas-bloques-models"
+
 export const username = 'USERNAME'
 export const password = 'PASSWORD'
 export const parentDNI = 'DNI'
 
-export const userJson = {
+export const userJson: Partial<User> = {
   username: username.toLowerCase(),
-  password,
   parentName: 'string',
   parentDNI,
   profile: {
