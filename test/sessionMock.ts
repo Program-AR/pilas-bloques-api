3e38dd6c1311a3ad25a0aa9c03b31af92600f8b2
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2021-07-15 15:34:48 -0300

    fixing userJson

--- test/sessionMock.ts
+++ test/sessionMock.ts
@@ -4,8 +4,9 @@ export const username = 'USERNAME'
 export const password = 'PASSWORD'
 export const parentDNI = 'DNI'
 
-export const userJson: Partial<User> = {
+export const userJson = {
   username: username.toLowerCase(),
+  password,
   parentName: 'string',
   parentDNI,
   profile: {
